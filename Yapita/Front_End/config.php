838e9787bdb5400be7a67539bcb6e929d0e8b333
bobbyilham1 <dmpbfp@gmail.com>
2016-11-27 14:01:20 +0700

    Add files via upload

---
+++ Yapita/Front_End/config.php
@@ -0,0 +1,14 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '';
+
+// this configuration path for website
+define('PATH', 'http://localhost:53613/weyapita/Front End'); // isi path dari website anda
+define('SITE_URL', PATH . 'index.php');
+define('POSITION_URL', PATH . '?page=' . $page);
+
+// this configuration for database website
+define('DB_HOST', 'localhost'); // host yang di gunakan
+define('DB_USERNAME', 'root'); // username host
+define('DB_PASSWORD', ''); // password host
+define('DB_NAME', 'front'); // database yang di gunakan
+?>
