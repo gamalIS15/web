50f49919f95f7aa429e102f56162f8090afed915
Gamal Akbar <gamaltn@gmail.com>
2016-11-27 10:21:23 +0700

    Add files via upload

---
+++ Yapita/Front_End/modules/views/template.view.php
@@ -0,0 +1,24 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page']) ?
+$_GET['page'] : '';
+?>
+<!DOCTYPE html>
+<!--[if IE 8]>><html lang="en" class="ie8"><![endif]-->
+<!--[if IE 9]>><html lang="en" class="ie8"><![endif]-->
+<!--[if !IE ]>><!-->><html lang="en"><!--<![endif]-->
+    <head>
+    <meta charset="utf-8" />
+    <title>SMA YAPITA</title>
+
+    <!--CSS-->>
+    <link href="resource/css/bootstrap.min.css"
+     rel="stylesheet" />
+    <link href="resource/css/jquery.dataTables.min.css"
+     rel="stylesheet" />
+    <link href="resource/css/style.min.css"
+     rel="stylesheet" />
+
+    <!--Javascript-->>
+
+    </head>
+</html>
