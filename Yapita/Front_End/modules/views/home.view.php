46ac2f39c5cfb808736a66a43142604ad67b77ff
Gamal Akbar <gamaltn@gmail.com>
2016-11-27 21:13:24 +0700

    Add files via upload

---
+++ Yapita/Front_End/modules/views/home.view.php
@@ -0,0 +1,29 @@
+<!--INFORMSI TERBSRU-->
+<div class="middle-panel">
+    <div class="top-middle-panel">Informasi Terbaru</div>
+    <div class="bottom-middle-panel">
+        <ul>
+            <?php
+                foreach ($data["artikel"] as $artikel){
+            ?>
+            <li>
+                <div class="date">
+                    <?php echo $artikel->waktu;?> - <?php echo date("d/m/Y", strtotime($artikel->tanggal));?>
+                </div>
+                <div class="title">
+                    <a href="<?php echo SITE_URL; ?>?page=artikel&&action=detail&&id=<?php echo 
+                    $artikel->id_artikel; ?><?php echo $artikel->judul;?>  "></a>
+                </div>
+                <div class="text">
+                    <?php echo substr(strip_tags($artikel->isi), 0,250);?>
+                </div>
+            </li>
+            <?php
+                }
+            ?>
+        </ul>
+    </div>
+    <div class="link">
+        <a class="btn btn-primary" href="<?php echo SITE_URL;?>?page=artikel">Tampilkan Semua Berita</a>
+    </div>
+</div>
