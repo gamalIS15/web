1fad3735f2ba4d89c5b1633aed633a2f1486030b
Gamal Akbar <gamaltn@gmail.com>
2016-11-27 12:25:33 +0700

    Add files via upload

--- Yapita/Front_End/modules/views/template.view.php
+++ Yapita/Front_End/modules/views/template.view.php
@@ -11,14 +11,51 @@ $_GET['page'] : '';
     <title>SMA YAPITA</title>
 
     <!--CSS-->>
-    <link href="resource/css/bootstrap.min.css"
+    <link href="resources/css/bootstrap.min.css"
      rel="stylesheet" />
-    <link href="resource/css/jquery.dataTables.min.css"
+    <link href="resources/css/jquery.dataTables.min.css"
      rel="stylesheet" />
-    <link href="resource/css/style.min.css"
+    <link href="resources/css/style.min.css"
      rel="stylesheet" />
 
     <!--Javascript-->>
+    <script type="text/javascript" src="resources/js/jquery.min.js"></script>
+    <script type="text/javascript" src="resources/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="resources/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="resources/js/expand.js"></script>
+    <script type="text/javascript" src="resources/js/common.js"></script>
 
     </head>
+    <body>
+    <!--Main Website-->>
+        <div id="main">
+
+            <!--Header Website-->>
+            <div id="Header">
+            <!--Logo-->>
+                <div id="logo">
+                <h1>SMA YAPITA</h1>
+                <h2>Keputih, Surabaya</h2>
+                </div>
+            </div>
+
+            <!--Sosmed-->>
+            <div class="social">
+            <ul>
+                <li><a href="#"><img src="resources/images/Facebook.png" alt="facebook"></a></li>
+                <li><a href="#"><img src="resources/images/Twitter.png" alt="twitter"></a></li>
+            </ul>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <!--Akhir Header-->>
+        <div id="top-menu-website">
+            <div class="left-side" style="margin-top: 48px;"></div>
+            <div class="middle-side">
+            <ul>
+            
+            </ul>
+            </div>
+        </div>
+    </body>
 </html>
