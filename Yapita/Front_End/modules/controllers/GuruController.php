27475b02dfb0668c3059c93c549eac77e6c25b37
bobbyilham1 <dmpbfp@gmail.com>
2016-12-02 09:27:24 +0700

    Create GuruController.php

---
+++ Yapita/Front_End/modules/controllers/GuruController.php
@@ -0,0 +1,14 @@
+<?php
+use \modules\controllers\MainController;
+
+class GuruController extends MainController {
+   
+   public function index() {
+      $this->model('guru');
+      
+      $data = $this->guru->get();
+      
+      $this->template('guru', array('guru' => $data));
+   }
+}
+?>
