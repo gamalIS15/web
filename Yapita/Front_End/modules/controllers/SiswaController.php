6e439d7946761ad3ec3aac9b44d3b3bc14563bd9
gilangDimizza <cupuway@gmail.com>
2016-12-05 16:40:17 +0700

    Update SiswaController.php

--- Yapita/Front_End/modules/controllers/SiswaController.php
+++ Yapita/Front_End/modules/controllers/SiswaController.php
@@ -17,24 +17,5 @@ class SiswaController extends MainController {
         
         $this->template('siswa', array('siswa' => $data));
     }
-    
-    //untuk halaman detail siswa
-    public function detail() {
-        
-        $id = isset($_GET["id"]) ? $_GET["id"] : "0";
-        
-        $this->model('siswa');
-        
-        $data = $this->siswa->getJoin('jurusan',
-            array(
-                'jurusan.id_jurusan' => 'siswa.id_jurusan'
-            ),
-            'JOIN',
-            array(
-                'id_siswa' => $id
-        );
-        
-        $this->template('detailsiswa', array('siswa' => $data));
-    }
 }
 ?>
