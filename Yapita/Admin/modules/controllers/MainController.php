7e7c1e79e6cd68eb22c5a78767717953cade042b
Gamal Akbar <gamaltn@gmail.com>
2016-12-05 15:05:42 +0700

    Add files via upload

--- Yapita/Admin/modules/controllers/MainController.php
+++ Yapita/Admin/modules/controllers/MainController.php
@@ -1,29 +1,28 @@
 <?php
-namespace modules\controllers;
-use modules\controllers;
 
-class MainController extends Controller{
-    
-    protected function template($viewName, $data = array()){
-        $this->model('artikel');
-        $artikel = $this->artikel->get(
-          array(
-              'limit' => '0,5'
-          )      
-        );
-        
-        $this->model('kategori');
-        $kategori = $this->kategori->get(
-          array(
-              'limit' => '0,5'
-          )        
-        );
-         $view = $this->view('template');
-         $view->bind('viewName', $viewName);
-         $view->bind('data', array_merge($data, array('main_artikel' => $artikel,
-             'main_kategori' => $kategori )));
+
+namespace modules\controllers;
+use \Controller;
+
+class MainController extends Controller {
+
+    protected $login;
+
+    public function __construct() {
+
+        $this->login = isset($_SESSION["login"]) ? $_SESSION["login"] : '';
+
+        if(!$this->login) {
+
+            $this->redirect(SITE_URL . "?page=login");
+        }
+    }
+
+    protected function template($viewName, $data = array()) {
+
+        $view = $this->view('template');
+        $view->bind('viewName', $viewName);
+        $view->bind('data', array_merge($data, array('login' => $this->login)));
     }
 }
-
 ?>
-
