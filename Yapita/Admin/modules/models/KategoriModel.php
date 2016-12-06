8c951d3b410cfe535644356c2488a2f062e64df2
bobbyilham1 <dmpbfp@gmail.com>
2016-12-06 23:00:01 +0700

    Create KategoriModel.php

---
+++ Yapita/Admin/modules/models/KategoriModel.php
@@ -0,0 +1,17 @@
+<?php
+class KategoriModel extends Model{
+    protected $tableName = "Kategori";
+    public function get($params = "") {
+        $data = array();
+        $kategori = $this->db->getAll($this->tableName)->toObject();
+        
+              foreach($kategori as $val) {
+                  $total = $this->db->getWhere('artikel', array('id_kategori' => $val->id_kategori))->numRows();
+                  $val->total = $total;
+                  
+                  array_push($data, $val);
+              }
+              return $data;
+        }
+}
+?>
