45f410d4b9da3c9dd1508fe597df3e2b6dbf54de
Gamal Akbar <gamaltn@gmail.com>
2016-12-08 14:31:19 +0700

    Add files via upload

--- Yapita/Admin/config.php
+++ Yapita/Admin/config.php
@@ -2,13 +2,13 @@
 $page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '';
 
 // this configuration path for website
-define('PATH', ''); // isi path dari website anda
+define('PATH', 'http://localhost:8080/Back_End/'); // isi path dari website anda
 define('SITE_URL', PATH . 'index.php');
 define('POSITION_URL', PATH . '?page=' . $page);
 
 // this configuration for database website
-define('DB_HOST', 'localhost:53613'); // host yang di gunakan
-define('DB_USERNAME', 'yapita'); // username host
-define('DB_PASSWORD', 'adgjmptw12'); // password host
-define('DB_NAME', 'yapita'); // database yang di gunakan
+define('DB_HOST', 'localhost'); // host yang di gunakan
+define('DB_USERNAME', 'root'); // username host
+define('DB_PASSWORD', ''); // password host
+define('DB_NAME', 'front'); // database yang di gunakan
 ?>
