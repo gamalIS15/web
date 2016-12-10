237ca3d8626f6cdb71c1615c58d79d9fb86c608b
bobbyilham1 <dmpbfp@gmail.com>
2016-12-10 22:17:59 +0700

    Create frmKategori.view.php

---
+++ Yapita/Admin/modules/views/frmKategori.view.php
@@ -0,0 +1,41 @@
+<div class="row">
+<div class="col-lg-12">
+<h1><?php echo $data["title"]; ?></h1>
+<ol class="breadcrumb">
+<li><a href="<?php SITE_URL; ?>">
+<i class="fa fa-dashboard"></i></a></li>
+<li><a href="<?php SITE_URL; ?>?page=kategori">
+<i class="fa fa-th-large"></i> Kategori</a></li>
+<li class="active"><i class="fa fa-pencil"></i>
+<?php echo $data["title"]; ?></li>
+</ol>
+</div>
+</div>
+
+<div class="row">
+<div class="col-lg-12">
+<?php
+    if(isset($data["error"]) && count($data["error"]) > 0) {
+?>
+<div class="alert alert-danger" role="alert">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<ul class="list-square">
+<?php
+      foreach($data["error"] as $error) {
+?>
+<li>
+<?php echo $error; ?>
+</li>
+<?php } ?>
+
+</ul>
+</div>
+<?php
+        }else if(isset($data["success"])) {
+?>
+
+<div class="alert alert-success">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<?php echo $data["success"]; ?>
+</div>
+<meta http-equiv="refresh" content="1;url=<?php echo PATH; ?>
