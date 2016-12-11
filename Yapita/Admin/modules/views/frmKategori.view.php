64257b8c77605a3052ac5645e8344494e4cf1718
bobbyilham1 <dmpbfp@gmail.com>
2016-12-11 10:46:49 +0700

    Update frmKategori.view.php

--- Yapita/Admin/modules/views/frmKategori.view.php
+++ Yapita/Admin/modules/views/frmKategori.view.php
@@ -39,3 +39,31 @@
 <?php echo $data["success"]; ?>
 </div>
 <meta http-equiv="refresh" content="1;url=<?php echo PATH; ?>
+?page=kategori">
+<?php }?>
+    
+<form method="post" role="form">
+<table class="table-responsive table">
+
+<tbody>
+<tr>
+
+    <td style="width: 200px;"><label>Nama Kategori</label></td>
+    <td style="width: 1px;">:</td>
+    <td>
+    <input type="text" name="kategori" <?php if(isset($data["kategori"])) echo 'value="'$data["kategori"]->nama_kategori . '"'; ?> class="form-control">
+        
+    </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td><button type="submit" class="btn btn-primary">Submit</button><a class="btn btn-warning" href="<?php echo PATH; ?>?page=kategori">Tampilkan Semua Kategori</a></td>
+    </tr>
+    </tbody>
+    
+    </table>
+    </form>
+    
+    </div>
+</div>
