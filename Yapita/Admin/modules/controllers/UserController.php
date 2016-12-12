a96bc32f856601adcca66cc6e286d3ae88fc8fe0
Gamal Akbar <gamaltn@gmail.com>
2016-12-12 20:54:37 +0700

    Update UserController.php

--- Yapita/Admin/modules/controllers/UserController.php
+++ Yapita/Admin/modules/controllers/UserController.php
@@ -45,5 +45,87 @@ class UserController extends MainController {
         }
 
     }
+	
+	    public function insert() {
+
+        $this->model('user');
+
+        $error      = array();
+        $success    = null;
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+            $nama           = isset($_POST["nama_lengkap"]) ? $_POST["nama_lengkap"]  : "";
+            $email          = isset($_POST["email"])        ? $_POST["email"]         : "";
+            $noHP           = isset($_POST["no_hp"])        ? $_POST["no_hp"]         : "";
+            $alamat         = isset($_POST["alamat"])       ? $_POST["alamat"]        : "";
+            $username       = isset($_POST["username"])     ? $_POST["username"]      : "";
+            $password       = isset($_POST["password"])     ? $_POST["password"]      : "";
+            $rePassword     = isset($_POST["re_password"])  ? $_POST["re_password"]   : "";
+
+            $foto           = isset($_FILES["images"])      ? $_FILES["images"]       : "";
+
+            if(empty($nama) || $nama == "") {
+
+                array_push($error, "Nama harus di isi.");
+            }
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                array_push($error, "Format e-mail salah.");
+            }
+            if(empty($username) || $username == "") {
+
+                array_push($error, "Username harus di isi.");
+            }
+            if(empty($password) || $password == "") {
+
+                array_push($error, "Password harus di isi.");
+            }
+            if($password != $rePassword) {
+
+                array_push($error, "Password dan Re-Type Password harus sama.");
+            }
+
+            if(!empty($foto["name"]) && $foto["type"] != 'image/jpg' && $foto["type"] != 'image/jpeg' && $foto["type"] != 'image/png') {
+                array_push($error, "Gambar hanya boleh .JPG/.PNG");
+            }
+
+            if(count($error) == 0) {
+
+                $imageName = $foto["name"];
+
+                if($foto["name"]) {
+
+                    $imageName = date("h_i_s_Y_m_d_") . str_replace(" ","_", $nama) . '.jpg';
+
+                    move_uploaded_file($foto["tmp_name"], '../public/images/user/' . $imageName);
+                }
+
+                $insert = $this->user->insert(
+
+                    array(
+                        'nama_lengkap'  => $nama,
+                        'email'         => $email,
+                        'no_hp'         => $noHP,
+                        'alamat'        => $alamat,
+                        'username'      => $username,
+                        'password'      => md5($password),
+                        'images'        => $imageName
+                    )
+                );
+
+                if($insert) {
+
+                    $success = "Data Berhasil di simpan.";
+                }
+            }
+
+
+        }
+
+        $this->template('frmUser', array('error' => $error, 'success' => $success, 'title' => 'Tambah User'));
+
+    }		
 }
 ?>
