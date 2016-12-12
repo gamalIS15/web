b7badbb733a3078aa253ada53828662f450b98c7
Gamal Akbar <gamaltn@gmail.com>
2016-12-12 21:05:26 +0700

    Update GuruController.php

--- Yapita/Admin/modules/controllers/GuruController.php
+++ Yapita/Admin/modules/controllers/GuruController.php
@@ -26,5 +26,26 @@ class GuruController extends MainController {
         $this->template('detailGuru', array('guru' => $data[0]));
 
     }
+   
+   public function delete() {
+
+        $id = isset($_GET["id"]) ? $_GET["id"] : 0;
+
+        $this->model('guru');
+
+        $data = $this->guru->getWhere(array(
+            'id_guru'  => $id
+        ));
+
+        $delete = $this->guru->delete(array('id_guru' => $id));
+
+
+        if($delete && $data[0]->images) {
+            unlink('../public/images/guru/' . $data[0]->images);
+          $this->back();
+        }
+
+    }
+
 }
 ?>
