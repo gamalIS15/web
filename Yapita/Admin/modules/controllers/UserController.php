96fcf0f66d04d2c548834e0eebfc502bb81145c1
Gamal Akbar <gamaltn@gmail.com>
2016-12-12 20:57:12 +0700

    Update UserController.php

--- Yapita/Admin/modules/controllers/UserController.php
+++ Yapita/Admin/modules/controllers/UserController.php
@@ -127,5 +127,97 @@ class UserController extends MainController {
         $this->template('frmUser', array('error' => $error, 'success' => $success, 'title' => 'Tambah User'));
 
     }		
+	
+    public function update() {
+
+        $id = isset($_GET["id"]) ? $_GET["id"] : '0';
+
+        $this->model('user');
+
+        $data = $this->user->getWhere(
+            array(
+                'id_user'  => $id
+            )
+        );
+
+        $error      = array();
+        $success    = null;
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+            $nama           = isset($_POST["nama_lengkap"]) ? $_POST["nama_lengkap"] : "";
+            $email          = isset($_POST["email"])        ? $_POST["email"]        : "";
+            $noHP           = isset($_POST["no_hp"])        ? $_POST["no_hp"]        : "";
+            $alamat         = isset($_POST["alamat"])       ? $_POST["alamat"]       : "";
+            $password       = isset($_POST["password"])     ? $_POST["password"]     : "";
+            $rePassword     = isset($_POST["re_password"])  ? $_POST["re_password"]  : "";
+
+            $foto           = isset($_FILES["images"])      ? $_FILES["images"]      : "";
+
+            if(empty($nama) || $nama == "") {
+
+                array_push($error, "Nama harus di isi.");
+            }
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                array_push($error, "Format e-mail salah.");
+            }
+
+            if(!empty($foto["name"]) && $foto["type"] != 'image/jpg' && $foto["type"] != 'image/jpeg' && $foto["type"] != 'image/png') {
+                array_push($error, "Gambar hanya boleh .JPG/.PNG");
+            }
+
+            if(count($error) == 0) {
+
+
+                $imageName = $foto["name"];
+
+                $dataUpdate = array(
+                    'nama_lengkap'  => $nama,
+                    'email'         => $email,
+                    'no_hp'         => $noHP,
+                    'alamat'        => $alamat
+                );
+
+                if($foto["name"]) {
+
+                    $imageName = date("h_i_s_Y_m_d_") . str_replace(" ","_", $nama) . '.jpg';
+
+                    unlink('../public/images/user/' . $data[0]->images);
+                    move_uploaded_file($foto["tmp_name"], '../public/images/user/' . $imageName);
+
+                    $dataUpdate["images"] = $imageName;
+                }
+
+                if(isset($password) && $password != "") {
+					
+                    if($rePassword == "" || $password != $rePassword) {
+
+                        array_push($error, "Password dan Re-Type Password harus sama.");
+
+                    }else {
+
+                        $dataUpdate["password"] = generateHash($password); 
+                    }
+                }
+
+                if(count($error) == 0) {
+
+                    $update = $this->user->update($dataUpdate, array('id_user' => $id));
+
+                    if($update) {
+
+                        $success = "Data Berhasil di simpan.";
+                    }
+                }
+            }
+
+
+        }
+
+        $this->template('frmUser', array('user' => $data[0], 'error' => $error, 'success' => $success, 'title' => 'Update User'));
+
+    }	
 }
 ?>
