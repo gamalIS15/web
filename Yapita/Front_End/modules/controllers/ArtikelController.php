c7fca9ab7d5c9894a83c8150124013401cc8bd32
Gamal Akbar <gamaltn@gmail.com>
2016-12-14 15:02:35 +0700

    Add files via upload

---
+++ Yapita/Front_End/modules/controllers/ArtikelController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @Author  : David Naista<davidnaista83@gmail.com>
+ * @Date    : 12/28/15 - 3:54 PM
+ */
+
+use \modules\controllers\MainController;
+
+class ArtikelController extends MainController{
+
+    public function index() {
+
+        $this->model('artikel');
+
+        $data = $this->artikel->get();
+
+        $this->template('artikel', array('artikel' => $data));
+    }
+
+    // untuk menampilkan detail artikel
+
+    public function detail() {
+
+        $id = isset($_GET["id"]) ? $_GET["id"] : "0";
+
+        $this->model('artikel');
+
+        $artikel = $this->artikel->getWhere(array('id_artikel' => $id));
+
+        $prev    = $this->artikel->getPrev($id);
+        $next    = $this->artikel->getNext($id);
+
+        $this->template('detailartikel', array('artikel' => $artikel, 'prev' => $prev, 'next' => $next));
+
+    }
+
+}
+?>
--- Yapita/Front_End/modules/controllers/DetailguruController.php
+++ Yapita/Front_End/modules/controllers/DetailguruController.php
@@ -1,14 +1,21 @@
 <?php
+
+
 use \modules\controllers\MainController;
+
 class DetailguruController extends MainController {
-  public function index() {
-      $id = isset($_GET["id"]) ? $_GET["id"] : "0";
-      
-      $this->model('guru');
-      
-      $data = $this->guru->getWhere(array('id_guru' => $id));
-      
-              $this->template('detailguru', array('guru' => $data));
-      }
+
+    public function index() {
+
+
+        $id = isset($_GET["id"]) ? $_GET["id"] : "0";
+
+        $this->model('guru');
+
+        $data = $this->guru->getWhere(array('id_guru' => $id));
+
+        $this->template('detailguru', array('guru' => $data));
+    }
+
 }
 ?>
